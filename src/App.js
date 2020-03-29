eab28b087048f4ad18bfb1d2ad6b39bea383a1e0
Anjali Sharma <anjalishar89@gmail.com>
2020-03-29 12:01:31 -0700

    Addds updated files and folders

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import FriendCard from "./components/FriendCard";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import Container from "./Container";
-import Row from "./Row";
-import Column from "./Column";
+import Container from ".components/Container";
+import Row from ".components/Row";
+import Column from ".components/Column";
 import friends from "./friends.json";
 import "./App.css";
 
