4f67e5ffb467dcf907a7d0af0f69f116be364969
Anjali Sharma <anjalishar89@gmail.com>
2020-03-30 13:06:09 -0700

    Adds a build folder

---
+++ build/precache-manifest.4fe1cd045c2418d2106dfe6562e829a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7993cb97be262a370f9a36ccb2268ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0df72362dc549a3992a",
+    "url": "/static/css/main.23ccad54.chunk.css"
+  },
+  {
+    "revision": "f5deaf57cc09011eee0c",
+    "url": "/static/js/2.dfe0b1b1.chunk.js"
+  },
+  {
+    "revision": "1d549b11cb9a0d87f2345e055e3cada2",
+    "url": "/static/js/2.dfe0b1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0df72362dc549a3992a",
+    "url": "/static/js/main.6b80665d.chunk.js"
+  },
+  {
+    "revision": "ab86c4c028b57ce476fc",
+    "url": "/static/js/runtime-main.07ed10b3.js"
+  }
+]);
