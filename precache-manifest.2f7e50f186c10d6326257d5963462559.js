02c1ba90e33abf31c3d847dc86b1b47dd4a9d71a
Anjali Sharma <anjalishar89@gmail.com>
2020-03-30 13:13:46 -0700

    Updates

---
+++ precache-manifest.2f7e50f186c10d6326257d5963462559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91673a7c5f1b9c6c36f25923e2445290",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d0df72362dc549a3992a",
+    "url": "/clicky-game/static/css/main.23ccad54.chunk.css"
+  },
+  {
+    "revision": "f5deaf57cc09011eee0c",
+    "url": "/clicky-game/static/js/2.dfe0b1b1.chunk.js"
+  },
+  {
+    "revision": "1d549b11cb9a0d87f2345e055e3cada2",
+    "url": "/clicky-game/static/js/2.dfe0b1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0df72362dc549a3992a",
+    "url": "/clicky-game/static/js/main.6b80665d.chunk.js"
+  },
+  {
+    "revision": "84f183f776dec52d4ec4",
+    "url": "/clicky-game/static/js/runtime-main.5c9b0e5c.js"
+  }
+]);
